cbdaeef5f0b39a107f587c4856374207f14d0e67
alexl <alexl@LAPTOP-K5F5FDFE>
2024-04-16 19:41:00 +0200

    Primera pujada al repositori

---
+++ Examen_M5_Alex_Lopez/src/main/java/com/mycompany/examen_m5_alex_lopez/Examen_M5_Alex_Lopez.java
@@ -0,0 +1,8 @@
+package com.mycompany.examen_m5_alex_lopez;
+
+public class Examen_M5_Alex_Lopez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
